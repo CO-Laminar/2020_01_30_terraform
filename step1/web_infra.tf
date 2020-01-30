0f5ea3ba983c796ae5bd18fc22b0f71b99261ad8
CO-Laminar <laminar@comtec.co.kr>
2020-01-30 14:07:11 +0900

    Creating EC2 Instance

--- step1/web_infra.tf
+++ step1/web_infra.tf
@@ -5,8 +5,31 @@ resource "aws_key_pair" "web_admin" {
   public_key = file("~/.ssh/web_admin.pub")
 }
 
+resource "aws_security_group" "ssh" {
+  name = "allow_ssh_from_all"
+  description = "Allow SSH port from all"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
+data "aws_security_group" "default" {
+  name = "default"
+  vpc_id = "vpc-f2c9e695"
+}
 
+resource "aws_instance" "web" {
+  ami = "ami-05c64f7b4062b0a21"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.web_admin.key_name
+  vpc_security_group_ids = [
+    aws_security_group.ssh.id,
+    data.aws_security_group.default.id
+  ]
+}
 
 // old version
 resource "aws_key_pair" "web_admin" {
